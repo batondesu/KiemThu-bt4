41a6b7d9af604f75a2115ccbca264b3c1e329501
batondesu <trantoan29072003@gmail.com>
2024-04-03 22:23:51 +0700

    VER2

--- main/Banking.java
+++ main/Banking.java
@@ -2,7 +2,7 @@ package main;
 public class Banking { 
 
     public static int Savemoney(int x, int y) {
-        if (x <= 1) {
+        if (x < 1) {
             return y;
         }
         while(x > 1) {
